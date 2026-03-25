73a9ca59213f350f762bc52ace08bb775e833bb4
Fabio Landin <flandin1990@gmail.com>
2026-03-25 11:49:47 -0300

    Configurações iniciais do projeto

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom"
+import { Button } from "./ui/button"
+import { CheckSquare2Icon, PlusIcon, ShoppingCart } from "lucide-react"
+
+const Sidebar = () => {
+    return (
+        < div className = "bg-gray-900 min-w-60 p-4 flex flex-col gap-2" >
+            {/* Sideber */ }
+                <h1 className="text-white">Menu</h1>
+                <hr className="border-gray-700" />
+                
+                <Button asChild variant="link" className="text-sm text-white justify-start">
+                    <Link to="/contador">
+                    <PlusIcon size={18} />
+                    Contador
+                    </Link>
+                </Button>
+
+                <Button asChild variant="link" className="text-sm text-white justify-start">
+                    <Link to="/contador2">
+                    <PlusIcon size={18} />
+                    Contador Completo
+                    </Link>
+                </Button>
+
+                <Button asChild variant="link" className="text-sm text-white justify-start">
+                    <Link to="/">
+                    <CheckSquare2Icon size={18} />
+                    Tarefas
+                    </Link>
+                </Button>
+
+                <Button asChild variant="link" className="text-sm text-white justify-start">
+                    <Link to="/"> 
+                    <ShoppingCart size={18} />
+                    Compras
+                    </Link>
+                </Button>
+
+            </div >
+    )
+}
+export default Sidebar
--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -1,51 +1,51 @@
+import Sidebar from "@/components/Sidebar"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { CheckSquareIcon, PlusIcon, ShoppingCartIcon } from "lucide-react"
 
 const Menu = () => {
     return (
-        <div className="flex">
-            {/* Sideber */}
-            <div className="bg-gray-900 h-screen w-64 p-4 flex flex-col gap-2">
-                <h1 className="text-white">Menu</h1>
-                <hr className="border-gray-700" />
+        <div className="flex h-screen">
+            <Sidebar />
 
-                <Button variant="link" className="text-sm text-white justify-start">
-                    <PlusIcon size={18} />
-                    Contador
-                </Button>
-
-                <Button variant="link" className="text-sm text-white justify-start">
-                    <PlusIcon size={18} />
-                    Contador
-                </Button>
-                <Button variant="link" className="text-sm text-white justify-start">
-                    <CheckSquareIcon size={18} />
-                    Tarefas
-                </Button>
-
-                <Button variant="link" className="text-sm text-white justify-start">
-                    <ShoppingCartIcon size={18} />
-                    Compras
-                </Button>
-
-            </div>
-            <div className="bg-gray-200 w-full">
-                {/* Tabela */}
-                <Card className="w-[500px] justify-center">
+            {/* Card */}
+            <div className="w-full flex justify-center items-center">
+                <Card className="min-w-[90%]  p-2 justify-center">
                     <CardHeader>
-                        <CardTitle>Titulo de Card</CardTitle>
-                        <CardDescription>Descrição do card </CardDescription>
-                        <CardContent>
-                            <h1>Descrição</h1>
-                            <h1>Descrição</h1>
-                            <h1>Descrição</h1>
-                        </CardContent>
-                        <CardFooter className="flex">
-                            <Button className="flex-1">Button1</Button>
-                            <Button className="flex-1">Button2</Button>
-                        </CardFooter>
+                        <CardTitle>Card De dialogs</CardTitle>
+                        <CardDescription>Card para Abertura de Dialogs </CardDescription>
                     </CardHeader>
+                    <CardContent>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                        <h1>Descrição</h1>
+                    </CardContent>
+                    <CardFooter className="flex">
+                        <Button className="flex-1">Button1</Button>
+                        <Button className="flex-1">Button2</Button>
+                    </CardFooter>
                 </Card>
             </div>
         </div>
