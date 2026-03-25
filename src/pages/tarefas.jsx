a6fb6f09a20c9e46b1262f7c213a56bc5b9c8a43
Fabio Landin <flandin1990@gmail.com>
2026-03-25 18:28:38 -0300

    feat:Colocando dados nas listas de tarefas

--- src/pages/tarefas.jsx
+++ src/pages/tarefas.jsx
@@ -1,6 +1,13 @@
 import DialogTarefas from "@/components/DialogTarefas";
 import Sidebar from "@/components/Sidebar";
+import { listadeTarefas } from "@/tarefas";
+import { Check, CheckLineIcon, StopCircleIcon } from "lucide-react";
 const Tarefas = () => {
+
+    const dados = listadeTarefas
+
+    const tarefasEmAberto = dados.filter((tarefas) => tarefas.status === "Em aberto")
+    const tarefasFinalizadas = dados.filter((tarefas) => tarefas.status === "Finalizado")
 
     return (
         <div className="flex bg-[#FFFFFF]">
@@ -13,11 +20,31 @@ const Tarefas = () => {
                     <h1 className="font-bold text-slate-900">Tarefas há fazer</h1>
                     <DialogTarefas />
                 </div>
+                <div>
+                    <ul>
+                        {tarefasEmAberto.map((tarefa) => (
+                            <li className="flex p-1 mt-1 text-gray-900" key={tarefa.id}>
+                                <StopCircleIcon size={22} className="text-slate-900"/>
+                                {tarefa.nome} - {tarefa.descricao}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
             </div>
             {/* Tarefas Concluidas */}
             <div className="bg-white p-4 ml-10 mr-5 mt-10 mb-10 min-w-[500px] min-h-[500px] rounded-lg  border rounded-lg shadow-lg">
-                <h1 className="font-bold text-slate-900">Tarefas Concluidas</h1>
+                <h1 className="font-bold text-green-800">Tarefas Concluidas</h1>
+                <div>
+                    <ul>
+                        {tarefasFinalizadas.map((tarefa) => (
+                            <li className="flex p-1 mt-1 text-green-800" key={tarefa.id}>
+                                <Check size={22} className="text-green-800"/>
+                                {tarefa.nome} - {tarefa.descricao}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
 
         </div>
---
+++ src/tarefas.js
@@ -0,0 +1,12 @@
+export const listadeTarefas = [
+    {id: 1, nome: "Fazer TCC", descricao: "começar os documentos do TCC", status: "Finalizado"},
+    {id: 2, nome: "Comprar Roupas", descricao: "Comprar roupas online no site", status:"Em aberto"},
+    {id: 3, nome: "Estudar React", descricao: "Ver o curso da full stack Club", status:"Finalizado"},
+    {id: 4, nome: "Descansar", descricao: "Pausa para recuperar as energias", status:"Em aberto"},
+    {id: 5, nome: "Assistir Naruto", descricao: "Ver episódios da fase Shippuden", status:"Em aberto"},
+    {id: 6, nome: "Treinar Pernas", descricao: "Ir à academia fazer o treino B", status: "Em aberto"},
+    {id: 7, nome: "Revisar Pull Requests", descricao: "Analisar o código da equipe no GitHub", status: "Em aberto"},
+    {id: 8, nome: "Lavar o Carro", descricao: "Levar no posto ou lavar na garagem", status: "Finalizado"},
+    {id: 9, nome: "Organizar Mesa", descricao: "Limpar setup e organizar cabos", status: "Em aberto"},
+    {id: 10, nome: "Ler Livro Tecnológico", descricao: "Ler 10 páginas de 'Código Limpo'", status: "Finalizado"},
+];
